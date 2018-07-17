ce8e4882148e648c514e64743afc8db7694ffcd3
hendrik.tjabben <ht@buss-data.de>
2018-07-17 09:59:36 +0200

    Adds method to register pressed button

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 
 // Value of coin refused. For the used Coin Acceptor a value of 255 means that the inserted coin is refused
 const int NO_COIN=255;
+// Value of no button pressed
+const int NO_BUTTON_PRESSED = -1;
 // enum representing the 8 doors
 enum class Door {
 door1 = 1,
@@ -23,14 +25,25 @@ sold = 0,
 aborted = 1
 };
 
+// Number of Doors
+const int doorNumbers = 8;
+
 // rxPin, txPin
 SoftwareSerial coinSerial(2,3);
+// The pin to get reset Button pressed
+const int buttonResetPin = 5;
+// The pin to get abort button pressed
+const int buttonAbortPin = 6;
+// The latch pin for shift registers
+const int latchPin = 7;
 // The clockPin for shift registers
 const int clockPin = 8;
 // The dataPin for door shift register
-const int dataPin = 9;
+const int dataPinDoor = 9;
+// The dataPin for buttons shift register
+const int dataPinButtons = 10;
 // The servoPin to control the coinServo
-const int coinServoPin = 7;
+const int coinServoPin = 11;
 
 // Servo instance to control servo motor that controls coin rocker
 Servo coinServo;
@@ -50,8 +63,12 @@ void setup() {
     coinServo.attach(coinServoPin);
 
     // Set PinModes for each individual Pin
+    pinMode(buttonResetPin, INPUT);
+    pinMode(buttonAbortPin, INPUT);
+    pinMode(latchPin, OUTPUT);
     pinMode(clockPin, OUTPUT);
-    pinMode(dataPin, OUTPUT);
+    pinMode(dataPinDoor, OUTPUT);
+    pinMode(dataPinButtons, INPUT);
 }
 
 /**
@@ -82,6 +99,46 @@ void moveCoinServo(ServoRotation rotation) {
     coinServo.write(rotationValue);
     // Waits for the Servo to get there
     delay(50);
+}
+
+int getButtonPressed() {
+    int tmpButtonState = 0;
+    int pressedButton = 0;
+    
+    // Check reset Button pressed
+    if (digitalRead(buttonResetPin)) {
+        // TODO Return buttonReset
+        return 0;
+    }
+    //Check abort button pressed
+    if (digitalRead(buttonAbortPin)){
+        // TODO Return buttonAbort
+        return 0;
+    }
+
+    // set latch to LOW to collect parallel data
+    digitalWrite(latchPin, LOW);
+    // Wait for data to get collected
+    delayMicroseconds(20);
+    // Set latch to HIGH to transmit data serially
+    digitalWrite(latchPin, HIGH);
+
+    // Loop over all door buttons. Register transmits information about pins from pin 7 to
+    // pin 0. So the loop counts down
+    for (int i = doorNumbers - 1; i >=0; i--) {
+        // A low to high clock drop causes the shift register's data pin to change state based
+        // on value of the next bit in its serial information flow
+        digitalWrite(clockPin, LOW);
+        delayMicroseconds(0.2);
+        tmpButtonState = digitalRead(dataPinButtons);
+        if (tmpButtonState) {
+            // At this point, a door button was pressed
+            pressedButton |= (1 << i);
+            return resultDataIn;
+        }
+        digitalWrite(clockPin, HIGH);
+    }
+    return NO_BUTTON_PRESSED;
 }
 
 /**
