9951b69d5aa0a0b969728a62703f1debab89fae9
hendrik.tjabben <ht@buss-data.de>
2018-07-17 09:33:32 +0200

    Adds servo instance and method to rotate servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+#include <Servo.h>
 
 // Value of coin refused. For the used Coin Acceptor a value of 255 means that the inserted coin is refused
 const int NO_COIN=255;
 // enum representing the 8 doors
-enum class Doors {
+enum class Door {
 door1 = 1,
 door2 = 2,
 door3 = 3,
@@ -14,6 +15,13 @@ door6 = 32,
 door7 = 64,
 door8 = 128
 };
+// enum representing servo rotations
+enum class ServoRotations{
+// Rotation for the Servo if honey was sold
+sold = 0,
+// Rotation for the Servo if payment was aborted
+aborted = 1
+};
 
 // rxPin, txPin
 SoftwareSerial coinSerial(2,3);
@@ -21,6 +29,11 @@ SoftwareSerial coinSerial(2,3);
 const int clockPin = 8;
 // The dataPin for door shift register
 const int dataPin = 9;
+// The servoPin to control the coinServo
+const int coinServoPin = 7;
+
+// Servo instance to control servo motor that controls coin rocker
+Servo coinServo;
 
 /**
  * Sets up all variables for the program
@@ -33,6 +46,9 @@ void setup() {
     // Setup Serial Port for Coin Acceptor
     coinSerial.begin(4800);
 
+    // Attaches servo connected to coinServoPin to the servo object
+    coinServo.attach(coinServoPin);
+
     // Set PinModes for each individual Pin
     pinMode(clockPin, OUTPUT);
     pinMode(dataPin, OUTPUT);
@@ -40,17 +56,32 @@ void setup() {
 
 /**
  * opens or closes a specified door
- * @param doorNumber The Number of the addressed door. Please use enum 'Doors' for this
+ * @param door The door to open
  * @param doorState The state to set the door to. Either HIGH to open door or LOW to close it
  */
-void doorWrite(int doorNumber, int doorState) {
+void doorWrite(Door door, int doorState) {
    byte bitsToSend = 0;
+   // Convert door to int
+   int doorNumber = static_cast<int>(door);
 
    // Turn on the corresponding bit in bitsToSend
    bitWrite(bitsToSend, doorNumber, doorState);
 
-   // Shift out the bits to open door
+   // Shift out the bits to open/close door
    shiftOut(dataPin, clockPin, MSBFIRST, bitsToSend);
+}
+
+/**
+ * Rotates the coinServo
+ * @param rotation The rotation to rotate the servo at
+ */
+void moveCoinServo(ServoRotation rotation) {
+    //Convert rotation to int
+    int rotationValue = static_cast<int>(rotation);
+    // Sets the servo position
+    coinServo.write(rotationValue);
+    // Waits for the Servo to get there
+    delay(50);
 }
 
 /**
