02c3d6c502db0f160b4b33e2b5e0259eeae5b781
hendrik.tjabben <ht@buss-data.de>
2018-07-17 08:49:41 +0200

    Adds method to shift out bit to open/close door

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,24 @@
 
 // Value of coin refused. For the used Coin Acceptor a value of 255 means that the inserted coin is refused
 const int NO_COIN=255;
+// enum representing the 8 doors
+enum class Doors {
+door1 = 1,
+door2 = 2,
+door3 = 3,
+door4 = 8,
+door5 = 16,
+door6 = 32,
+door7 = 64,
+door8 = 128
+};
 
 // rxPin, txPin
 SoftwareSerial coinSerial(2,3);
+// The clockPin for shift registers
+const int clockPin = 8;
+// The dataPin for door shift register
+const int dataPin = 9;
 
 /**
  * Sets up all variables for the program
@@ -18,7 +33,24 @@ void setup() {
     // Setup Serial Port for Coin Acceptor
     coinSerial.begin(4800);
 
-    pinMode(13, OUTPUT);
+    // Set PinModes for each individual Pin
+    pinMode(clockPin, OUTPUT);
+    pinMode(dataPin, OUTPUT);
+}
+
+/**
+ * opens or closes a specified door
+ * @param doorNumber The Number of the addressed door. Please use enum 'Doors' for this
+ * @param doorState The state to set the door to. Either HIGH to open door or LOW to close it
+ */
+void doorWrite(int doorNumber, int doorState) {
+   byte bitsToSend = 0;
+
+   // Turn on the corresponding bit in bitsToSend
+   bitWrite(bitsToSend, doorNumber, doorState);
+
+   // Shift out the bits to open door
+   shiftOut(dataPin, clockPin, MSBFIRST, bitsToSend);
 }
 
 /**
@@ -44,6 +76,8 @@ int getInsertedCoin() {
     } 
     return NO_COIN;
 }
+
+
 
 /**
  * Main loop routine
